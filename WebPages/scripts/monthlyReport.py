f44fea43aaaded2ef1fe308f0fd7bfc14b50164a
Tommy Cook <Tommy_Cook@edwards.com>
2024-03-29 16:32:02 -0700

    More planning for monthlyReport

---
+++ WebPages/scripts/monthlyReport.py
@@ -0,0 +1,55 @@
+#Pull from database #Total Wo's for the month and the past quarter, so past 3 months also
+
+#Compare to last year Q1 and by month
+
+#Get total months
+
+#Total PMs
+
+#Monthly Wo's for the below categories
+
+# WO	1202
+# Mechanical	 
+# Electrical	30
+# Plumbing	3
+# Misc	274
+# Events	152
+# Moves	33
+ 	 
+# Duryea	0
+# 3009 Daimler	2
+# Daimler Warehouse	8
+# Kettering	1
+# 1901	21
+# 1921	17
+# Anton	2
+# COM	30
+# Building A	10
+# Building C	70
+# POD 1	25
+# POD 2	5
+# POD 3	17
+# POD 4	22
+# POD 5	6
+# POD 6	6
+# POD 7	35
+# ERC	19
+# HVC	7
+# MUSEUM	2
+# T&D	37
+# Starr Atrium	49
+# PRT	55
+# LINC	129
+# MLE	34
+# LFS	12
+# LFSX	6
+# All parking structures	12
+
+
+
+#Connect to datbase
+
+#Pull on index index_building_creation_date which will grab several things
+#Query all work orders form this date onwards, provided via dropdown in html page
+#Query all work orders past 3 months onwards, provded via dropdown in html page
+#Query 
