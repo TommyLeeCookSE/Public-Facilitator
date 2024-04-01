9813e9756757bf177472f249af2324f72c9be04c
Tommy Cook <Tommy_Cook@edwards.com>
2024-04-01 16:34:10 -0700

    Updated Badge HTML and py

--- WebPages/scripts/badgeAutomator.py
+++ WebPages/scripts/badgeAutomator.py
@@ -7,11 +7,19 @@ company_dict = {
     "MBO PARTNERS, INC"                             :   "MBO PARTNERS",
     "CLUNE CONSTRUCTION COMPANY L.P"                :   "CLUNE CONSTRUCTION",
     "REDBOCK LLC"                                   :   "REDBOCK",
+    "UNIVERSAL PROTECTION SERVICE, LP"              :   "ALLIED UNIVERSAL",
     "UNIVERSAL PROTECTION SERVICE, LP DBA"          :   "ALLIED UNIVERSAL",
-    "MINDLANCE, LLC"                                :   "MINDLANCE, LLC",
+    "MINDLANCE, LLC"                                :   "MINDLANCE",
+    "MINDLANCE, INC."                               :   "MINDLANCE",
     "BAXTER CREDIT UNION_UNITED STATES"             :   "BAXTER CREDIT UNION",
     "KELLY SERVICES PR_PUERTO RICO"                 :   "KELLY SERVICES",
-    "MINDLANCE, LLC"                                :   "MINDLANCE"
+    "MINDLANCE, LLC"                                :   "MINDLANCE",
+    "IST MANAGEMENT SERVICES"                       :   "IST MANAGEMENT",
+    "THE CLEANNERS_UNITED STATES"                   :   "THE CLEANERS",
+    "PRIVATE CAR WASH_UNITED STATES"                :   "PRIVATE CAR WASH",
+    "MAGNIT, LLC"                                   :   "MAGNIT",
+    "PLANET PHARMA GROUP, LLC"                      :   "PLANET PHARMA",
+
     }
 
 class Person:
@@ -30,8 +38,8 @@ class Person:
         self.clearance4         = "**Parking Lot Access 24/7"
 
     def check_company_name(self):
-        if self.company in company_dict:
-            self.company = company_dict[self.company]
+        if self.company.strip() in company_dict:
+            self.company = company_dict[self.company.strip()]
 
 
     def check_dept_num(self):
@@ -43,7 +51,7 @@ def process(data):
     split = [line.upper().strip().split('\t') for line in data.split('\n')]
     employee_list = []
     for i,item in enumerate(split):
-       employee_list.append(Person(split[i][0],split[i][1],split[i][2],split[i][3],split[i][4],split[i][5],split[i][6]))
+       employee_list.append(Person(split[i][0].strip(),split[i][1].strip(),split[i][2].strip(),split[i][3].strip(),split[i][4].strip(),split[i][5].strip(),split[i][6].strip()))
     
     for i,item in enumerate(employee_list):
         employee_list[i].check_company_name()
